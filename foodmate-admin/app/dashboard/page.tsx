0d8b6eb203304550ed8e3ef1d17f647c9a1e54ce
Yashar Aliabbasi <yasharaliabasi@gmail.com>
2025-04-30 22:16:59 +0300

    Register newly signed up user into our db

--- foodmate-admin/app/dashboard/page.tsx
+++ foodmate-admin/app/dashboard/page.tsx
@@ -1,19 +1,38 @@
-import { auth } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { PrismaClient } from "../generated/prisma";
 
 export default async function Dashboard() {
 	const db = new PrismaClient();
-	const users = await db.users.findMany();
-	const user = await auth();
+	const user = await currentUser();
+
 	if (!user) {
 		redirect("/sign-in");
 	}
 
+	const isNewUser =
+		user && Date.now() - new Date(user.createdAt).getTime() < 30_000;
+
+	if (isNewUser) {
+		console.log("New user detected, creating user in database...", user);
+		try {
+			await db.users.create({
+				data: {
+					clerkId: user.id ?? "",
+					email: user.emailAddresses[0].emailAddress ?? "",
+					firstName: user.firstName ?? "",
+					lastName: user.lastName ?? "",
+				},
+			});
+		} catch (error) {
+			console.error("Error creating user in database:", error);
+		}
+	}
+
 	return (
 		<div>
-			<p>{user.userId}</p>
-			<p>{JSON.stringify(users)}</p>
+			<p>{user.id}</p>
+			<p>{JSON.stringify(await db.users.findMany())}</p>
 		</div>
 	);
 }
