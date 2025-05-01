e3dc389494d6e5d831e8fa612cf97977d5be3ef2
Yashar Aliabbasi <yasharaliabasi@gmail.com>
2025-05-01 16:59:25 +0300

    Remove not used import

--- foodmate-admin/app/dashboard/page.tsx
+++ foodmate-admin/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import { redirect } from "next/navigation";
 import { info } from "@/lib/offlineLogger";
 import {
 	createOrUpdateUser,
-	createUser,
 	getAllUsers,
 } from "@/repositories/users/UserRepository";
 import { error } from "console";
