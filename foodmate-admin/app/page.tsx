619f0d169b4044308c57b2b3de59203c06cb2ab3
Yashar Aliabbasi <yasharaliabasi@gmail.com>
2025-05-06 10:21:05 +0300

    add checkRole method

--- foodmate-admin/app/page.tsx
+++ foodmate-admin/app/page.tsx
@@ -4,38 +4,48 @@ import { currentUser } from "@clerk/nextjs/server";
 import { serialize } from "cookie";
 
 export default async function Home() {
-	const cookieStore = await cookies();
-	
-		const user = await currentUser();
-		if (!user) {
-			return new Response(
-				JSON.stringify({ message: "User not found" }),
-				{
-					status: 401,
-					headers: { "Content-Type": "application/json" },
-				});
-		}
-		const serialized = serialize("jwt", signJwt(
-			{
-				ic: user.id,
-				roles: user.publicMetadata.roles || [],
-				firstName: user.firstName,
-				lastName: user.lastName,
-			}, process.env.JWT_EXPIRATION ? parseInt(process.env.JWT_EXPIRATION) : 60
-		), {
-			httpOnly: true,
-			maxAge: 60 * (process.env.JWT_EXPIRATION ? parseInt(process.env.JWT_EXPIRATION) : 60),
-			sameSite: "strict",
-			secure: process.env.NODE_ENV === "production",
-		});
-	
-		return new Response(
-			JSON.stringify({ message: JSON.stringify(cookieStore.getAll()) }),
-			{
-				status: 200,
-				headers: {
-					"Set-Cookie": serialized,
-					"Content-Type": "application/json",
-				},
-			});
+	// const cookieStore = await cookies();
+
+	// const user = await currentUser();
+	// if (!user) {
+	// 	return new Response(JSON.stringify({ message: "User not found" }), {
+	// 		status: 401,
+	// 		headers: { "Content-Type": "application/json" },
+	// 	});
+	// }
+	// const serialized = serialize(
+	// 	"jwt",
+	// 	signJwt(
+	// 		{
+	// 			ic: user.id,
+	// 			roles: user.publicMetadata.roles || [],
+	// 			firstName: user.firstName,
+	// 			lastName: user.lastName,
+	// 		},
+	// 		process.env.JWT_EXPIRATION
+	// 			? parseInt(process.env.JWT_EXPIRATION)
+	// 			: 60
+	// 	),
+	// 	{
+	// 		httpOnly: true,
+	// 		maxAge:
+	// 			60 *
+	// 			(process.env.JWT_EXPIRATION
+	// 				? parseInt(process.env.JWT_EXPIRATION)
+	// 				: 60),
+	// 		sameSite: "strict",
+	// 		secure: process.env.NODE_ENV === "production",
+	// 	}
+	// );
+
+	// return new Response(
+	// 	JSON.stringify({ message: JSON.stringify(cookieStore.getAll()) }),
+	// 	{
+	// 		status: 200,
+	// 		headers: {
+	// 			"Set-Cookie": serialized,
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	});
+	return <div>asdas</div>;
 }
