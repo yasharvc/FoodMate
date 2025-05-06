d96c85acdc99fd61dbce6467600f794193932b00
Yashar Aliabbasi <yasharaliabasi@gmail.com>
2025-05-06 10:43:29 +0300

    add shadcn and theme mode toggle button component

--- foodmate-admin/app/dashboard/page.tsx
+++ foodmate-admin/app/dashboard/page.tsx
@@ -32,6 +32,7 @@ export default async function Dashboard() {
 
 	return (
 		<div>
+			
 			<p>{user.id}</p>
 			<p>{JSON.stringify(await getAllUsers())}</p>
 		</div>
--- foodmate-admin/app/layout.tsx
+++ foodmate-admin/app/layout.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import { checkRoles } from "@/lib/auth";
+import { ThemeProvider } from "@/components/Theme/theme-provider";
+import { ThemeModeToggle } from "@/components/UI/theme-mode-toggle";
 
 const nunitoSans = Nunito({
 	variable: "--font-nunito-sans",
@@ -26,39 +28,47 @@ export default async function RootLayout({
 }>) {
 	return (
 		<ClerkProvider>
-			<html lang="en">
+			<html lang="en" suppressHydrationWarning>
 				<body
 					className={`${nunitoSans.variable} ${nunitoMono.variable} antialiased`}
 				>
-					<header className="flex justify-end items-center p-4 gap-4 h-16">
-						<SignedOut>
-							Signed out
-							{(await checkRoles(["UsER", "Admin"])) ? (
-								<div className="text-green-500">
-									User or admin
-								</div>
-							) : (
-								<div className="text-red-500">
-									Not user or admin
-								</div>
-							)}
-							{/* <SignInButton />
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<header className="flex justify-end items-center p-4 gap-4 h-16">
+							<SignedOut>
+								Signed out
+								{(await checkRoles(["UsER", "Admin"])) ? (
+									<div className="text-green-500">
+										User or admin
+									</div>
+								) : (
+									<div className="text-red-500">
+										Not user or admin
+									</div>
+								)}
+								{/* <SignInButton />
 							<SignUpButton /> */}
-						</SignedOut>
-						<SignedIn>
-							{(await checkRoles(["UsER", "Admin"])) ? (
-								<div className="text-green-500">
-									User or admin
-								</div>
-							) : (
-								<div className="text-red-500">
-									Not user or admin
-								</div>
-							)}
-							<UserButton />
-						</SignedIn>
-					</header>
-					{children}
+							</SignedOut>
+							<SignedIn>
+								{(await checkRoles(["UsER", "Admin"])) ? (
+									<div className="text-green-500">
+										User or admin
+									</div>
+								) : (
+									<div className="text-red-500">
+										Not user or admin
+									</div>
+								)}
+								<UserButton />
+								<ThemeModeToggle />
+							</SignedIn>
+						</header>
+						{children}
+					</ThemeProvider>
 				</body>
 			</html>
 		</ClerkProvider>
